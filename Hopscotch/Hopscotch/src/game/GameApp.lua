69b4cb6cb7fb7ee46cffeef0fc6d13adb31f582c
brj <153065642@qq.com>
2017-05-23 18:42:40 +0800

    新工程搭建

--- Hopscotch/Hopscotch/src/game/GameApp.lua
+++ Hopscotch/Hopscotch/src/game/GameApp.lua
@@ -3,10 +3,10 @@ require("launcher.config")
 require("cocos.init")
 require("framework.init")
 
-local GameApp = class("GameApp", cc.mvc.AppBase)
+local GameApp = class("GameApp", cc.mvc.AppBase)    --继承AppBase：app = self
 
 function GameApp:ctor()
---    GameApp.super.ctor(self)
+--    MyApp.super.ctor(self)
     GameApp.super.ctor(self,"GameApp","game")    --替换app目录
 end
 
--- Hopscotch/Hopscotch/src/launcher/config.lua
+++ Hopscotch/Hopscotch/src/launcher/config.lua
@@ -2,6 +2,12 @@
 -- 0 - disable debug info, 1 - less debug info, 2 - verbose debug info
 DEBUG = 1
 
+--mobile debug console on screen
+--and do not connect internet
+MOBILE_DEBUG = false
+--gm
+GM_STATE = true
+
 -- display FPS stats on screen
 DEBUG_FPS = true
 
@@ -15,11 +21,12 @@ LOAD_DEPRECATED_API = false
 LOAD_SHORTCODES_API = true
 
 -- screen orientation
-CONFIG_SCREEN_ORIENTATION = "landscape"
+CONFIG_SCREEN_ORIENTATION = "portrait"
 
 -- design resolution
-CONFIG_SCREEN_WIDTH  = 960
-CONFIG_SCREEN_HEIGHT = 640
+CONFIG_SCREEN_WIDTH  = 750
+CONFIG_SCREEN_HEIGHT = 1334
 
 -- auto scale mode
-CONFIG_SCREEN_AUTOSCALE = "FIXED_HEIGHT"
+CONFIG_SCREEN_AUTOSCALE = "FIXED_WIDTH"
+
