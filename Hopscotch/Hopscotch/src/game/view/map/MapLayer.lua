15745fe34ed89486f79c405d601f65a8dab344ac
brj <153065642@qq.com>
2017-08-07 14:37:17 +0800

    跳跃逻辑优化

--- Hopscotch/Hopscotch/src/game/view/map/MapLayer.lua
+++ Hopscotch/Hopscotch/src/game/view/map/MapLayer.lua
@@ -1027,9 +1027,9 @@ function MapLayer:onEnterFrame(dt)
     end
     
     --火箭道具第一种类型
-    if (GameController.isInState(PLAYER_STATE.Rocket) and self.m_player:getRocketState()==1) or GameController.isInState(PLAYER_STATE.StartRocket) then
+--    if (GameController.isInState(PLAYER_STATE.Rocket) and self.m_player:getRocketState()==1) or GameController.isInState(PLAYER_STATE.StartRocket) then
         self:CoreLogic()
-    end
+--    end
     
     if self.rocket then
         local cameraPos = cc.p(self.m_camera:getPosition())
